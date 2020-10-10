91b104e1ef47b6758bca61d0c2eebf38445af7d5
Anna Presnyakova <apresnyakova88@gmail.com>
2020-10-10 19:52:28 +0200

    fix workflow file for deploying the app

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/hello, world/i);
+  const linkElement = getByText(/hello, world1/i);
   expect(linkElement).toBeInTheDocument();
 });
